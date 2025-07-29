bd4dbf115e9bd045957e342c91514425e93e990b
Hyun0801 <scoke0801@gmail.com>
2025-07-29 21:20:31 +0900

    태그 추가

--- Source/PGShared/Shared/Tag/PGGamePlayTags.cpp
+++ Source/PGShared/Shared/Tag/PGGamePlayTags.cpp
@@ -12,6 +12,14 @@ namespace PGGamePlayTags
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Equip_Weapon, "InputTag.Equip.Weapon");
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_UnEquip_Weapon, "InputTag.UnEquip.Weapon");
 
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Skill_Normal, "InputTag.Skill_Normal");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Skill_Slot1, "InputTag.Skill_Slot1");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Skill_Slot2, "InputTag.Skill_Slot2");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Skill_Slot3, "InputTag.Skill_Slot3");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Skill_Slot4, "InputTag.Skill_Slot4");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Skill_Slot5, "InputTag.Skill_Slot5");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Skill_Slot6, "InputTag.Skill_Slot6");
+	
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Attack, "InputTag.Attack");
 	
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_MustBeHeld, "InputTag.MustBeHeld");
