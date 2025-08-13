bfd0eb3debdd02994d1eded145a04ac8154fb23f
Hyun0801 <scoke0801@gmail.com>
2025-08-13 23:12:43 +0900

    UI 부착 테스트중

---
+++ Source/PGUI/Widget/HUD/PGUIHudPlayerInfo.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PGUIHudPlayerInfo.h"
+
+void UPGUIHudPlayerInfo::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	// Player 정보 가져와야 한다...
+}
---
+++ Source/PGUI/Widget/HUD/PGUIHudPlayerInfo.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PGUI/Widget/Base/PGWidgetBase.h"
+#include "PGUIHudPlayerInfo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PGUI_API UPGUIHudPlayerInfo : public UPGWidgetBase
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "PG|UI", meta=(BindWidget))
+	class UTextBlock* LevelText;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "PG|UI", meta=(BindWidget))
+	class UPGWidgetBase* HpBar;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "PG|UI", meta=(BindWidget))
+	class UImage* PlayerImage;
+
+protected:
+	virtual void NativeOnInitialized() override;
+};
