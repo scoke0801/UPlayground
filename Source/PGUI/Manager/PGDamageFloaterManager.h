ef7c8da2ba5a2a145cd230f690fa748471141fb6
Hyun0801 <scoke0801@gmail.com>
2025-08-20 20:23:06 +0900

    데미지 플로터 처리 위한 기본 클래스 추가

---
+++ Source/PGUI/Manager/PGDamageFloaterManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "PGDamageFloaterManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PGUI_API UPGDamageFloaterManager : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/PGUI/Widget/DamageFloater/PGUIDamageFloater.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PGUI/Widget/Base/PGWidgetBase.h"
+#include "PGUIDamageFloater.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PGUI_API UPGUIDamageFloater : public UPGWidgetBase
+{
+	GENERATED_BODY()
+	
+};
