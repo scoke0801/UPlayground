8a0270cfc656c3029b15027dcc34734a0e4f210a
Hyun0801 <scoke0801@gmail.com>
2025-08-26 23:21:23 +0900

    데칼 액터 베이스 클래스 추가

---
+++ Source/PGActor/Effects/Decal/PGSkillIndicator.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PGDecalActor.h"
+#include "PGDecalActor.h"
+#include "PGSkillIndicator.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PGACTOR_API APGSkillIndicator : public APGDecalActor
+{
+	GENERATED_BODY()
+	
+};
