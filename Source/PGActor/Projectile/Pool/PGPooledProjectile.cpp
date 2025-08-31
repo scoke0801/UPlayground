beef6ceedcf6bf4ca1fd4084daecd2026fab9acc
Hyun0801 <scoke0801@gmail.com>
2025-08-31 22:42:54 +0900

    투사체 풀링, 무브먼트 초기화 코드 수정

--- Source/PGActor/Projectile/Pool/PGPooledProjectile.cpp
+++ Source/PGActor/Projectile/Pool/PGPooledProjectile.cpp
@@ -5,9 +5,22 @@
 
 #include "PGProjectilePool.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "PGShared/Shared/Debug/PGDebugHelper.h"
 
 void APGPooledProjectile::Fire(const FVector& StartLocation, const FVector& Direction, float Speed, float InDamage)
 {
+	// MovementComponent 재활성화
+	if (MovementComponent)
+	{
+		MovementComponent->Activate();
+		MovementComponent->SetUpdatedComponent(RootComponent);
+	}
+
+	FString Msg = FString::Printf(TEXT("This: %p, Loc (%f, %f, %f), Dir (%f, %f, %f)"), this,
+		StartLocation.X, StartLocation.Y, StartLocation.Z,
+		Direction.X, Direction.Y, Direction.Z);
+	
+	PG_Debug::Print(Msg);
 	Super::Fire(StartLocation, Direction, Speed, InDamage);
 	
 	bInUse = true;
@@ -27,7 +40,14 @@ void APGPooledProjectile::OnLifeTimeExpired()
 void APGPooledProjectile::ReturnToPool()
 {
 	if (!bInUse) return;
-
+	
+	// MovementComponent 안전하게 정지
+	if (MovementComponent)
+	{
+		MovementComponent->StopMovementImmediately();
+		MovementComponent->Deactivate();
+	}
+	
 	SetActorHiddenInGame(true);
 	SetActorEnableCollision(false);
 	MovementComponent->StopMovementImmediately();
