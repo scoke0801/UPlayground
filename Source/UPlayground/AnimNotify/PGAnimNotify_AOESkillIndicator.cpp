0b115cfb26bd404cdd75efbad07df3f2576f7fac
Hyun0801 <scoke0801@gmail.com>
2025-10-07 17:23:08 +0900

    Indicator가 끝나는 시점에 이펙트 생성하도록 수정

--- Source/UPlayground/AnimNotify/PGAnimNotify_AOESkillIndicator.cpp
+++ Source/UPlayground/AnimNotify/PGAnimNotify_AOESkillIndicator.cpp
@@ -3,18 +3,34 @@
 
 #include "PGAnimNotify_AOESkillIndicator.h"
 
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Character.h"
+#include "Components/CapsuleComponent.h"
 #include "PGDataTableManager.h"
 #include "DataTable/Skill/PGSkillIndicatorDataRow.h"
 #include "PGActor/Effects/Decal/PGSkillIndicator.h"
+#include "PGActor/AreaOfEffect/PGAreaOfEffectBase.h"
 #include "Animation/AnimInstance.h"
 #include "PGActor/Components/Combat/PGSkillMontageController.h"
+
+void UPGAOESpawnHelper::OnIndicatorCompleted(APGSkillIndicator* Indicator)
+{
+	if (nullptr == Indicator || !OwnerActor.IsValid() || EffectId <= 0)
+	{
+		return;
+	}
+
+	// 인디케이터 위치에 AOE 스폰
+	FVector AOELocation = Indicator->GetActorLocation();
+	APGAreaOfEffectBase::Fire(OwnerActor.Get(), AOELocation, EffectId);
+}
 
 void UPGAnimNotify_AOESkillIndicator::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
                                           const FAnimNotifyEventReference& EventReference)
 {
 	Super::Notify(MeshComp, Animation, EventReference);
 
-	// 데이터테이블에서 ID값을 찾아서 생성
 	UPGDataTableManager* DataTableManager = UPGDataTableManager::Get();
 	if (nullptr == DataTableManager || nullptr == MeshComp)
 	{
@@ -27,48 +43,120 @@ void UPGAnimNotify_AOESkillIndicator::Notify(USkeletalMeshComponent* MeshComp, U
 		return;
 	}
 	
-	if (FPGSkillIndicatorDataRow* Data = DataTableManager->GetRowData<FPGSkillIndicatorDataRow>(SkillIndicatorId))
+	FPGSkillIndicatorDataRow* Data = DataTableManager->GetRowData<FPGSkillIndicatorDataRow>(SkillIndicatorId);
+	if (nullptr == Data)
 	{
-		// 클래스 로드
-		if (UClass* LoadedClass = Data->IndicatorPath.TryLoadClass<AActor>())
-		{
-			UWorld* World = MeshComp->GetWorld();
-			if (nullptr == World)
-			{
-				return;
-			}
-			// 스폰 위치 계산 (소유자 앞쪽)
-			FVector OwnerLocation = OwnerActor->GetActorLocation();
-			FVector OwnerForward = OwnerActor->GetActorForwardVector();
-			
-			FVector SpawnLocation = OwnerLocation;
-   
-			if (APGSkillIndicator* SpawnedActor = World->SpawnActor<APGSkillIndicator>(LoadedClass,
-				SpawnLocation, OwnerActor->GetActorRightVector().Rotation()))
-			{
-				UPGSkillMontageController* MontageController = OwnerActor->FindComponentByClass<UPGSkillMontageController>();
-				if (MontageController)
-				{
-					// 현재 재생 중인 몽타쥬 가져오기
-					if (UAnimInstance* AnimInstance = MeshComp->GetAnimInstance())
-					{
-						UAnimMontage* CurrentMontage = AnimInstance->GetCurrentActiveMontage();
-						if (CurrentMontage)
-						{
-							// 몽타쥬 컨트롤러에 인디케이터 등록
-							MontageController->RegisterSkillIndicator(SpawnedActor, CurrentMontage);
-						}
-					}
-				}
+		return;
+	}
 
-				if (false == DecalSize.IsNearlyZero())
-				{
-					SpawnedActor->SetDecalSize(DecalSize);
-				}
-				SpawnedActor->SetAnimationDuration(IndicatorDuration);
-				SpawnedActor->StartAnimation();
+	UClass* LoadedClass = Data->IndicatorPath.TryLoadClass<AActor>();
+	if (nullptr == LoadedClass)
+	{
+		return;
+	}
+
+	UWorld* World = MeshComp->GetWorld();
+	if (nullptr == World)
+	{
+		return;
+	}
+
+	// 타겟 액터 가져오기
+	AActor* TargetActor = GetTargetActor(OwnerActor);
+	if (nullptr == TargetActor)
+	{
+		return;
+	}
+
+	// 타겟의 바닥 위치 계산
+	FVector SpawnLocation = GetGroundLocation(TargetActor);
+
+	// 인디케이터 스폰
+	APGSkillIndicator* SpawnedIndicator = World->SpawnActor<APGSkillIndicator>(
+		LoadedClass,
+		SpawnLocation, 
+		FRotator::ZeroRotator
+	);
+
+	if (nullptr == SpawnedIndicator)
+	{
+		return;
+	}
+
+	// 몽타쥬 컨트롤러에 인디케이터 등록
+	if (UPGSkillMontageController* MontageController = OwnerActor->FindComponentByClass<UPGSkillMontageController>())
+	{
+		if (UAnimInstance* AnimInstance = MeshComp->GetAnimInstance())
+		{
+			if (UAnimMontage* CurrentMontage = AnimInstance->GetCurrentActiveMontage())
+			{
+				MontageController->RegisterSkillIndicator(SpawnedIndicator, CurrentMontage);
 			}
 		}
 	}
+
+	// 인디케이터 설정
+	if (false == DecalSize.IsNearlyZero())
+	{
+		SpawnedIndicator->SetDecalSize(DecalSize);
+	}
+	SpawnedIndicator->SetAnimationDuration(IndicatorDuration);
+
+	// 인디케이터 완료 시 AOE 스폰 설정
+	if (AreaOfEffectId > 0)
+	{
+		UPGAOESpawnHelper* SpawnHelper = NewObject<UPGAOESpawnHelper>(SpawnedIndicator);
+		SpawnHelper->OwnerActor = OwnerActor;
+		SpawnHelper->EffectId = AreaOfEffectId;
+
+		SpawnedIndicator->OnAnimationCompleted.AddDynamic(SpawnHelper, &UPGAOESpawnHelper::OnIndicatorCompleted);
+	}
+
+	// 애니메이션 시작
+	SpawnedIndicator->StartAnimation();
 }
 
+AActor* UPGAnimNotify_AOESkillIndicator::GetTargetActor(AActor* OwnerActor) const
+{
+	if (nullptr == OwnerActor)
+	{
+		return nullptr;
+	}
+
+	// AI Controller에서 블랙보드 가져오기
+	AAIController* AIController = Cast<AAIController>(OwnerActor->GetInstigatorController());
+	if (nullptr == AIController)
+	{
+		return nullptr;
+	}
+
+	UBlackboardComponent* Blackboard = AIController->GetBlackboardComponent();
+	if (nullptr == Blackboard)
+	{
+		return nullptr;
+	}
+
+	// 블랙보드에서 타겟 액터 가져오기
+	return Cast<AActor>(Blackboard->GetValueAsObject(TargetActorKeyName));
+}
+
+FVector UPGAnimNotify_AOESkillIndicator::GetGroundLocation(AActor* TargetActor) const
+{
+	if (nullptr == TargetActor)
+	{
+		return FVector::ZeroVector;
+	}
+
+	FVector GroundLocation = TargetActor->GetActorLocation();
+
+	// Character인 경우 CapsuleComponent의 반 높이를 고려
+	if (ACharacter* Character = Cast<ACharacter>(TargetActor))
+	{
+		if (UCapsuleComponent* Capsule = Character->GetCapsuleComponent())
+		{
+			GroundLocation.Z -= Capsule->GetScaledCapsuleHalfHeight();
+		}
+	}
+
+	return GroundLocation;
+}
--- Source/UPlayground/AnimNotify/PGAnimNotify_AOESkillIndicator.h
+++ Source/UPlayground/AnimNotify/PGAnimNotify_AOESkillIndicator.h
@@ -7,9 +7,29 @@
 #include "PGAnimNotify_AOESkillIndicator.generated.h"
 
 class UPGSkillMontageController;
+class APGSkillIndicator;
 
 /**
- * 
+ * AOE 스폰 헬퍼 - 인디케이터 완료 시 AOE를 스폰하는 콜백 핸들러
+ */
+UCLASS()
+class UPLAYGROUND_API UPGAOESpawnHelper : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY()
+	TWeakObjectPtr<AActor> OwnerActor;
+	
+	int32 EffectId = 0;
+
+	UFUNCTION()
+	void OnIndicatorCompleted(APGSkillIndicator* Indicator);
+};
+
+/**
+ * AOE 스킬 인디케이터 애님 노티파이
+ * 스킬 타겟의 바닥 위치에 인디케이터를 생성하고, 애니메이션 완료 시 AOE 이펙트를 스폰합니다.
  */
 UCLASS()
 class UPLAYGROUND_API UPGAnimNotify_AOESkillIndicator : public UAnimNotify
@@ -17,16 +37,32 @@ class UPLAYGROUND_API UPGAnimNotify_AOESkillIndicator : public UAnimNotify
 	GENERATED_BODY()
 
 protected:
+	// 스킬 인디케이터 ID
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PG|AnimNotify")
 	int32 SkillIndicatorId;
 
+	// 인디케이터 지속 시간
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PG|AnimNotify")
 	float IndicatorDuration = 1.0f;
 	
-protected:
+	// 데칼 크기
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PG|Spawn Settings")
 	FVector DecalSize = FVector::ZeroVector;
 
+	// 인디케이터 완료 후 스폰할 AOE 이펙트 ID
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PG|AOE Settings")
+	int32 AreaOfEffectId = 0;
+
+	// 타겟을 저장한 블랙보드 키 이름
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PG|Target Settings")
+	FName TargetActorKeyName = FName("TargetActor");
+
 private:
 	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+
+	// 타겟 액터 위치 가져오기
+	AActor* GetTargetActor(AActor* OwnerActor) const;
+	
+	// 타겟의 바닥 위치 계산
+	FVector GetGroundLocation(AActor* TargetActor) const;
 };
