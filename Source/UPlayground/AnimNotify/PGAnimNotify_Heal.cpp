c406e1b2e680e43b8c3ff399103fc0bdad0229df
Hyun0801 <scoke0801@gmail.com>
2025-10-14 22:24:33 +0900

    힐링 스킬 노티파이 추가

---
+++ Source/UPlayground/AnimNotify/PGAnimNotify_Heal.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimNotify/PGAnimNotify_Heal.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "PGActor/Characters/PGCharacterBase.h"
+
+UPGAnimNotify_Heal::UPGAnimNotify_Heal()
+{
+	SkillTargetActorKey.SelectedKeyName = FName("SkillTargetActor");
+}
+
+void UPGAnimNotify_Heal::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+
+	APGCharacterBase* Character = Cast<APGCharacterBase>(MeshComp->GetOwner());
+	if (nullptr == Character)
+	{
+		return;
+	}
+	AAIController* AIController = Cast<AAIController>(Character->GetInstigatorController());
+	if (nullptr == AIController)
+	{
+		return;
+	}
+
+	UBlackboardComponent* Blackboard = AIController->GetBlackboardComponent();
+	if (nullptr == Blackboard)
+	{
+		return;
+	}
+
+	// 블랙보드에서 타겟 액터 가져오기
+	APGCharacterBase* TargetActor = Cast<APGCharacterBase>(Blackboard->GetValueAsObject(SkillTargetActorKey.SelectedKeyName));
+	if (nullptr == TargetActor)
+	{
+		return;
+	}
+}
